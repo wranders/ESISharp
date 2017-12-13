d9f50601aba34d5b9a5e421a3536e6574092f04b
W Anders <wr_anders@yahoo.com>
2017-12-13 03:33:47 -0600

    Update

--- ESISharp/Authenticated.cs
+++ ESISharp/Authenticated.cs
@@ -7,8 +7,6 @@ namespace ESISharp
     {
         internal new Access Access = Access.Authenticated;
 
-        public Controller Sso;
-
         private Authenticated() : base()
         {
 
@@ -16,12 +14,12 @@ namespace ESISharp
 
         public Authenticated(string clientid) : this()
         {
-            Sso = new Controller(clientid);
+
         }
 
         public Authenticated(string clientid, string secretkey) : this()
         {
-            Sso = new Controller(clientid, secretkey);
+
         }
     }
 }
