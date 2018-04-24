f862e261a7cd7529dda7431d661d946154bbfc04
W Anders <wr_anders@yahoo.com>
2018-04-24 00:21:15 -0500

    Progress

--- ESISharp/Enumeration/SearchCategory.cs
+++ ESISharp/Enumeration/SearchCategory.cs
@@ -4,16 +4,16 @@
     {
         internal SearchCategory(string value) : base(value) { }
 
-        public static readonly SearchCategory All = new SearchCategory("agent,alliance,character,constellation,corporation,faction,inventorytype,region,solarsystem,station,wormhole");
+        public static readonly SearchCategory All = new SearchCategory("agent,alliance,character,constellation,corporation,faction,inventory_type,region,solar_system,station,wormhole");
         public static readonly SearchCategory Agent = new SearchCategory("agent");
         public static readonly SearchCategory Alliance = new SearchCategory("alliance");
         public static readonly SearchCategory Character = new SearchCategory("character");
         public static readonly SearchCategory Constellation = new SearchCategory("constellation");
         public static readonly SearchCategory Corporation = new SearchCategory("corporation");
         public static readonly SearchCategory Faction = new SearchCategory("faction");
-        public static readonly SearchCategory InventoryType = new SearchCategory("inventorytype");
+        public static readonly SearchCategory InventoryType = new SearchCategory("inventory_type");
         public static readonly SearchCategory Region = new SearchCategory("region");
-        public static readonly SearchCategory SolarSystem = new SearchCategory("solarsystem");
+        public static readonly SearchCategory SolarSystem = new SearchCategory("solar_system");
         public static readonly SearchCategory Station = new SearchCategory("station");
     }
 }
---
+++ ESISharp/Model/Object/EsiError.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace ESISharp.Model.Object
+{
+    public class EsiError
+    {
+        [JsonProperty(PropertyName = "error")]
+        public string Error { get; set; }
+
+        [JsonProperty(PropertyName = "sso_status")]
+        public int SsoStatus { get; set; }
+    }
+}
