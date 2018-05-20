750c81a7df2f7a5df6e483a59734911de52c3414
W Anders <wranders@protonmail.com>
2018-05-20 06:21:02 -0500

    Added Authed Search paths

--- ESISharp/Authenticated.cs
+++ ESISharp/Authenticated.cs
@@ -12,12 +12,14 @@ namespace ESISharp
         private readonly Paths.Authenticated.Characters.Main _Characters;
         private readonly Paths.Authenticated.Corporations.Main _Corporations;
         private readonly Paths.Authenticated.Fleets _Fleets;
+        private readonly Paths.Authenticated.Search _Search;
 
         public Sso.Client SsoClient => _SsoClient;
         public Paths.Authenticated.Alliances.Main Alliances => _Alliances;
         public Paths.Authenticated.Characters.Main Characters => _Characters;
         public Paths.Authenticated.Corporations.Main Corporations => _Corporations;
         public Paths.Authenticated.Fleets Fleets => _Fleets;
+        public Paths.Authenticated.Search Search => _Search;
 
         private Authenticated() : base()
         {
@@ -25,6 +27,7 @@ namespace ESISharp
             _Characters = new Paths.Authenticated.Characters.Main(this);
             _Corporations = new Paths.Authenticated.Corporations.Main(this);
             _Fleets = new Paths.Authenticated.Fleets(this);
+            _Search = new Paths.Authenticated.Search(this);
         }
 
         public Authenticated(string clientid) : this()
---
+++ ESISharp/Paths/Authenticated/Search.cs
@@ -0,0 +1,51 @@
+﻿using ESISharp.Enumeration;
+using ESISharp.Model.Abstract;
+using ESISharp.Model.Attribute;
+using ESISharp.Model.Object;
+using ESISharp.Web;
+using System.Collections.Generic;
+
+namespace ESISharp.Paths.Authenticated
+{
+    public class Search : ApiPath
+    {
+        internal Search(EsiConnection esiconnection) : base(esiconnection) { }
+
+        public EsiRequest SubString(int characterid, string search, SearchCategory category)
+            => SubString(characterid, search, new SearchCategory[] { category }, Language.English, false);
+
+        public EsiRequest SubString(int characterid, string search, IEnumerable<SearchCategory> categories)
+            => SubString(characterid, search, categories, Language.English, false);
+
+        public EsiRequest SubString(int characterid, string search, SearchCategory category, Language language)
+            => SubString(characterid, search, new SearchCategory[] { category }, language, false);
+
+        public EsiRequest SubString(int characterid, string search, IEnumerable<SearchCategory> categories, Language language)
+            => SubString(characterid, search, categories, language, false);
+
+        public EsiRequest SubString(int characterid, string search, SearchCategory category, bool strict)
+            => SubString(characterid, search, new SearchCategory[] { category }, Language.English, strict);
+
+        public EsiRequest SubString(int characterid, string search, IEnumerable<SearchCategory> categories, bool strict)
+            => SubString(characterid, search, categories, Language.English, strict);
+
+        public EsiRequest SubString(int characterid, string search, SearchCategory category, Language language, bool strict)
+            => SubString(characterid, search, new SearchCategory[] { category }, language, strict);
+
+        [Path("/characters/{character_id}/search/", WebMethods.GET)]
+        public EsiRequest SubString(int characterid, string search, IEnumerable<SearchCategory> categories, Language language, bool strict)
+        {
+            var path = new EsiRequestPath { "characters", characterid.ToString(), "search" };
+            var data = new EsiRequestData
+            {
+                Query = new Dictionary<string, dynamic>
+                {
+                    ["categories"] = categories,
+                    ["language"] = language.Value,
+                    ["strict"] = strict
+                }
+            };
+            return new EsiRequest(EsiConnection, path, WebMethods.GET, data);
+        }
+    }
+}
