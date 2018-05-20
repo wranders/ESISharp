3661450c985ca316ba94d28665b76eb337528fd4
W Anders <wranders@protonmail.com>
2018-05-20 06:18:03 -0500

    Added Fleets to main Authenticated paths class

--- ESISharp/Authenticated.cs
+++ ESISharp/Authenticated.cs
@@ -11,17 +11,20 @@ namespace ESISharp
         private readonly Paths.Authenticated.Alliances.Main _Alliances;
         private readonly Paths.Authenticated.Characters.Main _Characters;
         private readonly Paths.Authenticated.Corporations.Main _Corporations;
+        private readonly Paths.Authenticated.Fleets _Fleets;
 
         public Sso.Client SsoClient => _SsoClient;
         public Paths.Authenticated.Alliances.Main Alliances => _Alliances;
         public Paths.Authenticated.Characters.Main Characters => _Characters;
         public Paths.Authenticated.Corporations.Main Corporations => _Corporations;
+        public Paths.Authenticated.Fleets Fleets => _Fleets;
 
         private Authenticated() : base()
         {
             _Alliances = new Paths.Authenticated.Alliances.Main(this);
             _Characters = new Paths.Authenticated.Characters.Main(this);
             _Corporations = new Paths.Authenticated.Corporations.Main(this);
+            _Fleets = new Paths.Authenticated.Fleets(this);
         }
 
         public Authenticated(string clientid) : this()
