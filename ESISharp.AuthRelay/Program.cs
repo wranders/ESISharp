61fc3f1dbd80d4b420d973303cbf90bafba3d52f
W Anders <wranders@protonmail.com>
2018-06-11 22:00:07 -0500

    Added AuthRelay executable

---
+++ ESISharp.AuthRelay/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Web;
+
+namespace ESISharp.AuthRelay
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0 || args[0].IndexOf("state=", StringComparison.InvariantCulture) < 0)
+                return;
+
+            var Query = HttpUtility.ParseQueryString(args[0]);
+            NamedPipeClientStream Client = new NamedPipeClientStream(Query["state"]);
+            StreamWriter Writer = new StreamWriter(Client);
+
+            try
+            {
+                Client.Connect(10000);
+
+                Writer.WriteLine(args[0]);
+                Writer.Flush();
+
+                Client.WaitForPipeDrain();
+                Client.Close();
+            }
+            catch(TimeoutException)
+            {
+                Client.Dispose();
+            }
+            catch(IOException)
+            {
+                Client.Dispose();
+            }
+        }
+    }
+}
