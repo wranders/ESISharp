8d75301916e30fb572aba4f7fec94c659f2777f0
W Anders <wranders@protonmail.com>
2018-07-09 09:36:47 -0500

    Removed Corporation Outposts Scope

--- ESISharp/Sso/Scopes/Scope.Corporation.cs
+++ ESISharp/Sso/Scopes/Scope.Corporation.cs
@@ -12,7 +12,6 @@
             public static readonly Scope ReadFacilities = new Scope("esi-corporations.read_facilities.v1");
             public static readonly Scope ReadFactionWarfareStats = new Scope("esi-corporations.read_fw_stats.v1");
             public static readonly Scope ReadMedals = new Scope("esi-corporations.read_medals.v1");
-            public static readonly Scope ReadOutposts = new Scope("esi-corporations.read_outposts.v1");
             public static readonly Scope ReadStandings = new Scope("esi-corporations.read_standings.v1");
             public static readonly Scope ReadStarbases = new Scope("esi-corporations.read_starbases.v1");
             public static readonly Scope ReadStructures = new Scope("esi-corporations.read_structures.v1");
