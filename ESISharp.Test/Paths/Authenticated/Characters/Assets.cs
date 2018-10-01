b3f6f83c4f84773b55cc332d493d3eb3feade221
W Anders <wranders@protonmail.com>
2018-10-01 03:51:18 -0500

    Revert "Modified Assets test to be dynamic based on token bearer's character ID"
    This reverts commit b1a3cb13d29b3021303514e97d85b971272c277a.

--- ESISharp.Test/Paths/Authenticated/Characters/Assets.cs
+++ ESISharp.Test/Paths/Authenticated/Characters/Assets.cs
@@ -1,7 +1,5 @@
 ﻿using ESISharp.Test.Model.Abstract;
-using Newtonsoft.Json;
 using NUnit.Framework;
-using System;
 using System.Collections.Generic;
 using System.Net;
 
@@ -9,62 +7,68 @@ namespace ESISharp.Test.Paths.Authenticated.Characters
 {
     public class Assets : PathTest
     {
-        private long itemOne;
-        private long itemTwo;
-        private List<long> itemList;
+        private const int CharacterId = 91105772;
+#pragma warning disable S1144
 
-        private void PopulateItems()
+        static readonly object[] GetAll_One =
         {
-            if (itemOne != 0 && itemTwo != 0 && itemList.Count == 0) return;
+            new object[] { CharacterId, 2 },
+            new object[] { CharacterId, 3 }
+        };
 
-            var rand = new Random();
-            var r = Authenticated.Characters.Assets.GetAll(GetCharacterId()).Execute();
-            var b = JsonConvert.DeserializeObject<List<Dictionary<string, dynamic>>>(r.Body);
-            var c = b.Count;
-            var io = b[rand.Next(0, c - 1)];
-            var it = b[rand.Next(0, c - 1)];
-            itemOne = io["item_id"];
-            itemTwo = it["item_id"];
-            itemList = new List<long>() { itemOne, itemTwo };
-        }
+        static readonly object[] GetLocations_One =
+        {
+            new object[] { CharacterId, new List<long>() { 1002143262874, 1026459286466 } }
+        };
+
+#pragma warning restore
 
         [Property("AuthedCharacters", "Assets")]
-        [Test]
-        public void GetAll()
+        [TestCase(CharacterId)]
+        public void GetAll(int characterid)
         {
-            var a = Authenticated.Characters.Assets.GetAll(GetCharacterId()).Execute();
+            var a = Authenticated.Characters.Assets.GetAll(characterid).Execute();
             Assert.True(a.Code == HttpStatusCode.OK);
         }
 
         [Property("AuthedCharacters", "Assets")]
-        [TestCase(2)]
-        [TestCase(3)]
-        public void GetAll(int page)
+        [Test,TestCaseSource("GetAll_One")]
+        public void GetAll(int characterid, int page)
         {
-            var a = Authenticated.Characters.Assets.GetAll(GetCharacterId(), page).Execute();
+            var a = Authenticated.Characters.Assets.GetAll(characterid, page).Execute();
             Assert.True(a.Code == HttpStatusCode.OK);
         }
 
         [Property("AuthedCharacters", "Assets")]
-        [Test]
-        public void GetLocations()
+        [TestCase(CharacterId, 1026459286466)]
+        public void GetLocations(int characterid, long item)
         {
-            PopulateItems();
-            var a = Authenticated.Characters.Assets.GetLocations(GetCharacterId(), itemOne).Execute();
-            var b = Authenticated.Characters.Assets.GetLocations(GetCharacterId(), itemList).Execute();
+            var a = Authenticated.Characters.Assets.GetLocations(characterid, item).Execute();
             Assert.True(a.Code == HttpStatusCode.OK);
-            Assert.True(b.Code == HttpStatusCode.OK);
         }
 
         [Property("AuthedCharacters", "Assets")]
-        [Test]
-        public void GetNames()
+        [Test, TestCaseSource("GetLocations_One")]
+        public void GetLocations(int characterid, IEnumerable<long> item)
         {
-            PopulateItems();
-            var a = Authenticated.Characters.Assets.GetNames(GetCharacterId(), itemOne).Execute();
-            var b = Authenticated.Characters.Assets.GetNames(GetCharacterId(), itemList).Execute();
+            var a = Authenticated.Characters.Assets.GetLocations(characterid, item).Execute();
             Assert.True(a.Code == HttpStatusCode.OK);
-            Assert.True(b.Code == HttpStatusCode.OK);
+        }
+
+        [Property("AuthedCharacters", "Assets")]
+        [TestCase(CharacterId, 1026459286466)]
+        public void GetNames(int characterid, long item)
+        {
+            var a = Authenticated.Characters.Assets.GetNames(characterid, item).Execute();
+            Assert.True(a.Code == HttpStatusCode.OK);
+        }
+
+        [Property("AuthedCharacters", "Assets")]
+        [Test, TestCaseSource("GetLocations_One")]
+        public void GetNames(int characterid, IEnumerable<long> item)
+        {
+            var a = Authenticated.Characters.Assets.GetNames(characterid, item).Execute();
+            Assert.True(a.Code == HttpStatusCode.OK);
         }
     }
 }
