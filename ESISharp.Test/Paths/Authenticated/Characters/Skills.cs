7f7ea14aea9a8d91be8cea46c25a01f4d36356be
W Anders <wranders@protonmail.com>
2018-11-09 13:31:36 -0600

    Added Character Skills Tests

---
+++ ESISharp.Test/Paths/Authenticated/Characters/Skills.cs
@@ -0,0 +1,33 @@
+﻿using ESISharp.Test.Model.Abstract;
+using NUnit.Framework;
+using System.Net;
+
+namespace ESISharp.Test.Paths.Authenticated.Characters
+{
+    public class Skills : PathTest
+    {
+        [Property("AuthedCharacters", "Skills")]
+        [Test]
+        public void GetAttributes()
+        {
+            var a = Authenticated.Characters.Skills.GetAttributes(GetCharacterId()).Execute();
+            Assert.True(a.Code == HttpStatusCode.OK);
+        }
+
+        [Property("AuthedCharacters", "Skills")]
+        [Test]
+        public void GetSkillQueue()
+        {
+            var a = Authenticated.Characters.Skills.GetSkillQueue(GetCharacterId()).Execute();
+            Assert.True(a.Code == HttpStatusCode.OK);
+        }
+
+        [Property("AuthedCharacters", "Skills")]
+        [Test]
+        public void GetSkills()
+        {
+            var a = Authenticated.Characters.Skills.GetSkills(GetCharacterId()).Execute();
+            Assert.True(a.Code == HttpStatusCode.OK);
+        }
+    }
+}
