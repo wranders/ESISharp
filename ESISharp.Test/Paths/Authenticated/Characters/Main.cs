98f52141e20769021f1166029692c116097716e9
W Anders <wranders@protonmail.com>
2018-11-09 13:09:14 -0600

    Added Character Market Tests

--- ESISharp.Test/Paths/Authenticated/Characters/Main.cs
+++ ESISharp.Test/Paths/Authenticated/Characters/Main.cs
@@ -3,7 +3,6 @@ using NUnit.Framework;
 using System.Collections.Generic;
 using System.Net;
 
-
 namespace ESISharp.Test.Paths.Authenticated.Characters
 {
     public class Main : PathTest
---
+++ ESISharp.Test/Paths/Authenticated/Characters/Markets.cs
@@ -0,0 +1,34 @@
+﻿using ESISharp.Test.Model.Abstract;
+using NUnit.Framework;
+using System.Net;
+
+namespace ESISharp.Test.Paths.Authenticated.Characters
+{
+    public class Markets : PathTest
+    {
+        [Property("AuthedCharacters", "Main")]
+        [Test]
+        public void GetOpenOrders()
+        {
+            var a = Authenticated.Characters.Markets.GetOpenOrders(GetCharacterId()).Execute();
+            Assert.True(a.Code == HttpStatusCode.OK);
+        }
+
+        [Property("AuthedCharacters", "Main")]
+        [Test]
+        public void GetOrdersHistory()
+        {
+            var a = Authenticated.Characters.Markets.GetOrdersHistory(GetCharacterId()).Execute();
+            Assert.True(a.Code == HttpStatusCode.OK);
+        }
+
+        [Property("AuthedCharacters", "Main")]
+        [TestCase(1)]
+        [TestCase(2)]
+        public void GetOrdersHistory(int page)
+        {
+            var a = Authenticated.Characters.Markets.GetOrdersHistory(GetCharacterId(), page).Execute();
+            Assert.True(a.Code == HttpStatusCode.OK);
+        }
+    }
+}
