05b4d132aa23f9f8ad463550889c7263c9d34cb0
W Anders <wranders@protonmail.com>
2018-11-09 11:27:54 -0600

    Added Character Loyalty Tests

---
+++ ESISharp.Test/Paths/Authenticated/Characters/Loyalty.cs
@@ -0,0 +1,17 @@
+﻿using ESISharp.Test.Model.Abstract;
+using NUnit.Framework;
+using System.Net;
+
+namespace ESISharp.Test.Paths.Authenticated.Characters
+{
+    public class Loyalty : PathTest
+    {
+        [Property("AuthedCharacters", "Location")]
+        [Test]
+        public void GetPoints()
+        {
+            var a = Authenticated.Characters.Loyalty.GetPoints(GetCharacterId()).Execute();
+            Assert.True(a.Code == HttpStatusCode.OK);
+        }
+    }
+}
