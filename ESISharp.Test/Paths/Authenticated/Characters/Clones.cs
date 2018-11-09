ed2108afc748975df799232e2ebe46d175e9ed51
W Anders <wranders@protonmail.com>
2018-11-09 08:04:17 -0600

    Added Character Clone Tests

---
+++ ESISharp.Test/Paths/Authenticated/Characters/Clones.cs
@@ -0,0 +1,25 @@
+﻿using ESISharp.Test.Model.Abstract;
+using NUnit.Framework;
+using System.Net;
+
+namespace ESISharp.Test.Paths.Authenticated.Characters
+{
+    public class Clones : PathTest
+    {
+        [Property("AuthedCharacters", "Clones")]
+        [Test]
+        public void GetAll()
+        {
+            var a = Authenticated.Characters.Clones.GetAll(GetCharacterId()).Execute();
+            Assert.True(a.Code == HttpStatusCode.OK);
+        }
+
+        [Property("AuthedCharacters", "Clones")]
+        [Test]
+        public void GetActiveImplants()
+        {
+            var a = Authenticated.Characters.Clones.GetActiveImplants(GetCharacterId()).Execute();
+            Assert.True(a.Code == HttpStatusCode.OK);
+        }
+    }
+}
