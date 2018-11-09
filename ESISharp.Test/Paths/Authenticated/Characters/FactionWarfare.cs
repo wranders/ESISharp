72ce49e81a1f72f825d16d45bd2a4ee1847d1be1
W Anders <wranders@protonmail.com>
2018-11-09 10:19:07 -0600

    Added Character Faction Warfare Tests

---
+++ ESISharp.Test/Paths/Authenticated/Characters/FactionWarfare.cs
@@ -0,0 +1,17 @@
+﻿using ESISharp.Test.Model.Abstract;
+using NUnit.Framework;
+using System.Net;
+
+namespace ESISharp.Test.Paths.Authenticated.Characters
+{
+    public class FactionWarfare : PathTest
+    {
+        [Property("AuthedCharacters", "FactionWarfare")]
+        [Test]
+        public void GetOverview()
+        {
+            var a = Authenticated.Characters.FactionWarfare.GetOverview(GetCharacterId()).Execute();
+            Assert.True(a.Code == HttpStatusCode.OK);
+        }
+    }
+}
