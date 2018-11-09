8285b9f7ce94d820f254c87a9b4cd2f3e652fbc5
W Anders <wranders@protonmail.com>
2018-11-09 13:29:46 -0600

    Added Character Planetary Interaction Tests

---
+++ ESISharp.Test/Paths/Authenticated/Characters/PlanetaryInteraction.cs
@@ -0,0 +1,26 @@
+﻿using ESISharp.Test.Model.Abstract;
+using NUnit.Framework;
+using System.Net;
+
+namespace ESISharp.Test.Paths.Authenticated.Characters
+{
+    public class PlanetaryInteraction : PathTest
+    {
+        [Property("AuthedCharacters", "PlanetaryInteraction")]
+        [Test]
+        public void GetColonies()
+        {
+            var a = Authenticated.Characters.PlanetaryInteraction.GetColonies(GetCharacterId()).Execute();
+            Assert.True(a.Code == HttpStatusCode.OK);
+        }
+
+        [Untestable]
+        [Property("AuthedCharacters", "PlanetaryInteraction")]
+        [TestCase(99999999)]
+        public void GetColonyLayout(int planetid)
+        {
+            var a = Authenticated.Characters.PlanetaryInteraction.GetColonyLayout(GetCharacterId(), planetid).Execute();
+            Assert.True(a.Code == HttpStatusCode.OK);
+        }
+    }
+}
