e2ca4e4803b6d50d4ec601264c837ea48e06056a
W Anders <wranders@protonmail.com>
2019-05-08 00:04:54 -0500

    Consolidated AssemblyInfo instructions

--- ESISharp/Properties/AssemblyInfo.cs
+++ ESISharp/Properties/AssemblyInfo.cs
@@ -18,6 +18,7 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+[assembly: InternalsVisibleTo("ESISharp.Test")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("83c7add4-f276-4a38-a3a5-3e6640b2c28c")]
