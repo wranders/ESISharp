f85f106d0289af45a6820abf83434819b238f561
W Anders <wranders@protonmail.com>
2019-05-08 00:49:31 -0500

    Added Authenticated Market tests and Market updates

---
+++ ESISharp.Test/Paths/Authenticated/Markets.cs
@@ -0,0 +1,25 @@
+﻿using ESISharp.Test.Model.Abstract;
+using NUnit.Framework;
+using System.Net;
+
+namespace ESISharp.Test.Paths.Authenticated
+{
+    public class Markets : PathTest
+    {
+        [Property("Authenticated", "Markets")]
+        [TestCase(1030329520646)]
+        public void GetStructureOrders(long structureid)
+        {
+            var r = Authenticated.Markets.GetStructureOrders(structureid).Execute();
+            Assert.True(r.Code == HttpStatusCode.OK);
+        }
+
+        [Property("Authenticated", "Markets")]
+        [TestCase(1030329520646, 2)]
+        public void GetStructureOrders(long structureid, int page)
+        {
+            var r = Authenticated.Markets.GetStructureOrders(structureid, page).Execute();
+            Assert.True(r.Code == HttpStatusCode.OK);
+        }
+    }
+}
--- ESISharp/Paths/Authenticated/Markets.cs
+++ ESISharp/Paths/Authenticated/Markets.cs
@@ -3,6 +3,7 @@ using ESISharp.Model.Abstract;
 using ESISharp.Model.Attribute;
 using ESISharp.Model.Object;
 using ESISharp.Web;
+using System.Collections.Generic;
 
 namespace ESISharp.Paths.Authenticated
 {
@@ -10,10 +11,20 @@ namespace ESISharp.Paths.Authenticated
     {
         internal Markets(EsiConnection esiconnection) : base(esiconnection) { }
 
-        [Path("/markets/structures/{structure_id}/", WebMethods.GET)]
         public EsiRequest GetStructureOrders(long structureid)
+            => GetStructureOrders(structureid, 1);
+
+        [Path("/markets/structures/{structure_id}/", WebMethods.GET)]
+        public EsiRequest GetStructureOrders(long structureid, int page)
         {
             var path = new EsiRequestPath { "markets", "structures", structureid.ToString() };
+            var data = new EsiRequestData
+            {
+                Query = new Dictionary<string, dynamic>
+                {
+                    ["page"] = page
+                }
+            };
             return new EsiRequest(EsiConnection, path, WebMethods.GET);
         }
     }
