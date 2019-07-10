34f5596ec4359566cee160fed6efca29d369d802
W Anders <wranders@protonmail.com>
2019-07-10 03:14:27 -0500

    Disabled Citadel market test due to unreliability in CI

--- ESISharp.Test/Paths/Authenticated/Markets.cs
+++ ESISharp.Test/Paths/Authenticated/Markets.cs
@@ -6,16 +6,18 @@ namespace ESISharp.Test.Paths.Authenticated
 {
     public class Markets : PathTest
     {
+        [Untestable] // Public Citadel market access too unreliable
         [Property("Authenticated", "Markets")]
-        [TestCase(1030329520646)]
+        [TestCase(1000000000000)]
         public void GetStructureOrders(long structureid)
         {
             var r = Authenticated.Markets.GetStructureOrders(structureid).Execute();
             Assert.True(r.Code == HttpStatusCode.OK);
         }
 
+        [Untestable] // Public Citadel market access too unreliable
         [Property("Authenticated", "Markets")]
-        [TestCase(1030329520646, 2)]
+        [TestCase(1000000000000, 2)]
         public void GetStructureOrders(long structureid, int page)
         {
             var r = Authenticated.Markets.GetStructureOrders(structureid, page).Execute();
